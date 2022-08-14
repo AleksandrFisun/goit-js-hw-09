acfe838f89899b5f08c37d71534e54821d171a97
AleksandrFisun <99916190+AleksandrFisun@users.noreply.github.com>
2022-08-14 20:05:40 +0000

    Deploying to gh-pages from @ AleksandrFisun/goit-js-hw-09@7f02a8cd4f26fe6d07c1c24fb7a284cd0d029d51 🚀

--- 01-color-switcher.8b262d58.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={buttonStartRef:document.querySelector("button[data-start]"),buttonStopRef:document.querySelector("button[data-stop]")},n=null;t.buttonStartRef.addEventListener("click",(function(){t.buttonStartRef.disabled=!0,n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3)})),t.buttonStopRef.addEventListener("click",(function(){t.buttonStartRef.disabled=!1,clearInterval(n)}))}();
-//# sourceMappingURL=01-color-switcher.8b262d58.js.map
